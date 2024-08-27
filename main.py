0fffdc1cf35a65c39e72c326a56271ea4582db55
GroguLovesCookies <ninad.zambare@outlook.com>
2024-08-27 08:46:48 +0200

    Added piece collision to move generation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fen_parsers.chess_parser import ChessParser
 from move_generators.chess_move_generator import ChessMoveGenerator
 
 parser = ChessParser()
-board = parser.load("8/8/1N6/3r4/8/8/8/8")
+board = parser.load("8/8/3p4/8/4N3/21311/8/8")
 
 gen = ChessMoveGenerator(board)
 print(gen.generate())
--- move_generators/chess_move_generator.py
+++ move_generators/chess_move_generator.py
@@ -1,7 +1,7 @@
 from classes.board import Board
 from move_generators.move_generator import MoveGenerator
 from moves.chess_move import ChessMove
-from pieces import chess_pieces
+from pieces import chess_pieces, get_piece_value, get_piece_color
 from typing import List
 
 
@@ -11,16 +11,21 @@ def generate_sliding_moves(board: Board, coords: tuple) -> List[ChessMove]:
     distances: List[int] = board.get_distances(coords)
 
     use_offsets = range(8)
-    if board[coords] == chess_pieces["r"]:
+    if get_piece_value(board[coords]) == chess_pieces["r"]:
         use_offsets = [0, 2, 4, 6]
-    elif board[coords] == chess_pieces["b"]:
+    elif get_piece_value(board[coords]) == chess_pieces["b"]:
         use_offsets = [1, 3, 5, 7]
 
     for i in use_offsets:
         offset = offsets[i]
         for t in range(1, distances[i]):
             target_coord: tuple = (coords[0] + offset[0] * t, coords[1] + offset[1] * t)
+            piece_at_coord: int = board[target_coord]
+            if get_piece_color(board[coords]) == get_piece_color(piece_at_coord):
+                break
             moves.append(ChessMove(board, coords, target_coord))
+            if piece_at_coord != 0:
+                break
 
     return moves
 
@@ -33,16 +38,24 @@ def generate_knight_moves(board: Board, coords: tuple) -> List[ChessMove]:
             continue
         offset = offsets[i]
         branch_coord = (coords[0] + offset[0] * 2, coords[1] + offset[1] * 2)
+        end = None
         if offset[0] != 0:
             if distances[6] >= 2:
-                moves.append(ChessMove(board, coords,(branch_coord[0], branch_coord[1] - 1)))
-            if distances[2] >= 2:
-                moves.append(ChessMove(board, coords,(branch_coord[0], branch_coord[1] + 1)))
+                end = (branch_coord[0], branch_coord[1] - 1)
         else:
             if distances[4] >= 2:
-                moves.append(ChessMove(board, coords,(branch_coord[0] - 1, branch_coord[1])))
+                end = (branch_coord[0] - 1, branch_coord[1])
+        if board[end] == 0 or get_piece_color(board[end]) != get_piece_color(board[coords]):
+            moves.append(ChessMove(board, coords, end))
+
+        if offset[0] != 0:
+            if distances[2] >= 2:
+                end = (branch_coord[0], branch_coord[1] + 1)
+        else:
             if distances[0] >= 2:
-                moves.append(ChessMove(board, coords,(branch_coord[0] + 1, branch_coord[1])))
+                end = (branch_coord[0] + 1, branch_coord[1])
+        if board[end] == 0 or get_piece_color(board[end]) != get_piece_color(board[coords]):
+            moves.append(ChessMove(board, coords, end))
     return moves
 
 
@@ -56,6 +69,8 @@ def generate_king_moves(board: Board, coords: tuple) -> List[ChessMove]:
         offset = offsets[i]
         for t in range(1, min(distances[i], 2)):
             target_coord: tuple = (coords[0] + offset[0] * t, coords[1] + offset[1] * t)
+            if get_piece_color(board[coords]) == get_piece_color(board[target_coord]):
+                break
             moves.append(ChessMove(board, coords, target_coord))
 
     return moves
